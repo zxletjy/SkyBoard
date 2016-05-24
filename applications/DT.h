9a42a36faf84b5a8fd0561f885e7d46af163c3a0
TJY_GIT <tjy@flyeah.com>
2016-05-24 21:09:46 +0800

    通讯进程修改，节省内存

--- applications/DT.h
+++ applications/DT.h
@@ -6,17 +6,19 @@
 typedef struct
 {
 	uint16_t FailCheck;
-	u8 Send_Status;
-	u8 Send_Senser;
-	u8 Send_PID1;
-	u8 Send_PID2;
-	u8 Send_PID3;
-	u8 Send_RCData;
-	u8 Send_Offset;
-	u8 Send_MotoPwm;
-	u8 Send_Bat;
 }SKYBOARD_DT;
-
+enum
+{
+	SEND_STATES =0,
+	SEND_SENSER,
+	SEND_PID1,
+	SEND_PID2,
+	SEND_PID3,
+	SEND_RC,
+	SEND_OFFSET,
+	SEND_MOTORPWM,
+	SEND_BAT
+};
 void Data_Receive_Anl(u8 *data_buf,u8 num);
 //检查是否有接收到无线数据
 void Check_Event(void);
